fb8c2d0e999ce5a59e89db9b7b6ac7b02f2a0320
Nick Crews <nickcrews35@gmail.com>
2023-07-19 00:18:16 -0400

    Enhance CLI with file config

--- cli/app.py
+++ cli/app.py
@@ -1,20 +1,31 @@
+import json
 from engineer.run import Configuration, run
-from engineer.utils import prompt_string
+from engineer.utils import prompt_string, prompt_confirm
 
 
 def main():
-    repo = prompt_string("Repository URL:")
-    base_branch = prompt_string("Base Branch:", default="mainline")
-    dev_branch = prompt_string("Development Branch:", default="gpt-eng")
-    path = prompt_string("Path to directory/file:", default="/")
-    goal = prompt_string("Goal:")
 
-    run(
-        Configuration(
-            repo=repo,
-            base_branch=base_branch,
-            dev_branch=dev_branch,
-            path=path,
-            goal=goal,
+    read_from_file = prompt_confirm("Read from file?")
+
+    if read_from_file:
+        file_path = prompt_string("File path:", default="/")
+        with open(file_path, "r") as file:
+            config = json.load(file)
+
+        run(Configuration(**config))
+    else:
+        repository_url = prompt_string("Repository URL:")
+        base_branch = prompt_string("Base Branch:", default="mainline")
+        dev_branch = prompt_string("Development Branch:", default="gpt-eng")
+        path = prompt_string("Path to directory/file:", default="/")
+        goal = prompt_string("Goal:")
+
+        run(
+            Configuration(
+                repository_url=repository_url,
+                base_branch=base_branch,
+                dev_branch=dev_branch,
+                path=path,
+                goal=goal
+            )
         )
-    )
